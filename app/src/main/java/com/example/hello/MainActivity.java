e1113b36dd846d780ca30ed94ff79ab263ae3bb6
student <s1063707@pu.edu.tw>
2020-05-15 15:13:14 +0800

    加入dev分支2

--- app/src/main/java/com/example/hello/MainActivity.java
+++ app/src/main/java/com/example/hello/MainActivity.java
@@ -12,12 +12,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView txv = (TextView) findViewById(R.id.txv);
         txv.setText("Hello tcyang");
-        TestMaster();
+        TestDev();
     }
-    public void TestMaster(){
+    public void TestDev(){
         TextView txv = (TextView) findViewById(R.id.txv);
-        txv.setText("Master分支");
+        txv.setText("Dev分支");
     }
+
 
 
 }
